e2489e23b9ac04e1ac57b786aed6e9b93bebe639
Yatin <yatin@peerbits.com>
2020-11-19 19:33:36 +0530

    Updates

---
+++ precache-manifest.40b7dc4e1dc149cce9ab0ac454a2c8c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24910121154e44206fb19daa2b2327dc",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "179c9d1cfb44460f5335",
+    "url": "/react-demo/static/css/2.6e9815bd.chunk.css"
+  },
+  {
+    "revision": "c678f84bd1735cedf1ca",
+    "url": "/react-demo/static/css/main.75d95c24.chunk.css"
+  },
+  {
+    "revision": "179c9d1cfb44460f5335",
+    "url": "/react-demo/static/js/2.def7578d.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/react-demo/static/js/2.def7578d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c678f84bd1735cedf1ca",
+    "url": "/react-demo/static/js/main.3ddf40ff.chunk.js"
+  },
+  {
+    "revision": "c63e00b457bbb567fb4d",
+    "url": "/react-demo/static/js/runtime-main.31548a7c.js"
+  }
+]);
